978a5863d9a35c83cdc9c017365d7db59511fbd7
孙乾汇 <toby_sun@qq.com>
2018-01-17 16:14:20 +0800

    change mallet and puck from circle to cylinder; draw 3-dimensional table and wall; add gluLookAt

--- src/main.cpp
+++ src/main.cpp
@@ -117,15 +117,149 @@ void display(void) {
     //glEnable(GL_LIGHT0);
 
     //Draw Table
+	glBegin(GL_QUAD_STRIP);
+    glColor3f(0.4f, 0.4f, 0.4f);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), -2.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), -2.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), -2.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), -2.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), -2.0);
+	glEnd();
     glBegin(GL_QUADS);
-    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-K * TABLE_WIDTH / 2 + 0.1, -K * TABLE_LENGTH / 2 + 0.1, 0.0);
+    glVertex3f(K * TABLE_WIDTH / 2 + 0.1, -K * TABLE_LENGTH / 2 + 0.1, 0.0);
+    glVertex3f(K * TABLE_WIDTH / 2 + 0.1, K * TABLE_LENGTH / 2 + 0.1, 0.0);
+    glVertex3f(-K * TABLE_WIDTH / 2 + 0.1, K * TABLE_LENGTH / 2 + 0.1, 0.0);
+	glEnd();
+    glBegin(GL_QUADS);
+    glVertex3f(-K * TABLE_WIDTH / 2 + 0.1, -K * TABLE_LENGTH / 2 + 0.1, -2.0);
+    glVertex3f(K * TABLE_WIDTH / 2 + 0.1, -K * TABLE_LENGTH / 2 + 0.1, -2.0);
+    glVertex3f(K * TABLE_WIDTH / 2 + 0.1, K * TABLE_LENGTH / 2 + 0.1, -2.0);
+    glVertex3f(-K * TABLE_WIDTH / 2 + 0.1, K * TABLE_LENGTH / 2 + 0.1, -2.0);
+	glEnd();
+    glBegin(GL_QUADS);
+    glColor3f(0.07f, 0.52f, 0.02f);
     glVertex3f(-K * TABLE_WIDTH / 2, -K * TABLE_LENGTH / 2, 0.0);
     glVertex3f(K * TABLE_WIDTH / 2, -K * TABLE_LENGTH / 2, 0.0);
     glVertex3f(K * TABLE_WIDTH / 2, K * TABLE_LENGTH / 2, 0.0);
     glVertex3f(-K * TABLE_WIDTH / 2, K * TABLE_LENGTH / 2, 0.0);
     glEnd();
-    
-    //Draw Circle
+
+	//Draw Pillar
+	/*glBegin(GL_QUAD_STRIP);
+    glColor3f(0.4f, 0.4f, 0.4f);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 - 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 - 0.2), -4.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 + 0.2), -K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 + 0.2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 3 - 0.2), -K * (TABLE_LENGTH / 3 + 0.2), -4.0);
+	glEnd();*/
+
+
+	//Draw Wall
+	glBegin(GL_QUAD_STRIP);
+    glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+    glColor3f(0.7f, 0.7f, 0.7f);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (TABLE_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(K * (TABLE_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2 + 0.1), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2 + 0.1), 0.0);
+	glVertex3f(-K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2 + 0.1), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(-K * (GOAL_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2), -K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (TABLE_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(-K * (TABLE_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.0);
+	glVertex3f(-K * (GOAL_WIDTH / 2), K * (TABLE_LENGTH / 2), 0.1);
+	glEnd();
+
+
+/*    //Draw Circle
     glBegin(GL_POLYGON);
     glColor3f(0.0f, 1.0f, 0.5f);
     float ax = (a.x - TABLE_WIDTH / 2) * K, ay = (a.y - TABLE_LENGTH / 2) * K;
@@ -146,6 +280,70 @@ void display(void) {
     float bx = (b.x - TABLE_WIDTH / 2) * K, by = (b.y - TABLE_LENGTH / 2) * K;
     for(double i = 0; i < 2 * PI; i += PI / 24)
         glVertex3f(cos(i) * PUCK_DIAMETER * K / 2 + bx, sin(i) * PUCK_DIAMETER * K / 2 + by, 0.0);
+    glEnd();*/
+
+
+	//Draw Mallet1
+    glBegin(GL_QUAD_STRIP);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    float ax = (a.x - TABLE_WIDTH / 2) * K, ay = (a.y - TABLE_LENGTH / 2) * K;
+    for(double i = 0; i < 2 * PI; i += PI / 24)
+	{
+        glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + ax, sin(i) * MALLET_DIAMETER * K / 2 + ay, 0.0);
+		glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + ax, sin(i) * MALLET_DIAMETER * K / 2 + ay, 0.1);
+	}
+    glEnd();
+	glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.0f, 1.0f, 0.5f);
+    glVertex3f(ax, ay, 0.0f);
+    glVertex3f(ax, ay, 0.1f);
+    for (double i = 0; i <= 2 * PI; i += PI / 24)  
+    {  
+        glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + ax, sin(i) * MALLET_DIAMETER * K / 2 + ay, 0.0);
+		glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + ax, sin(i) * MALLET_DIAMETER * K / 2 + ay, 0.1);
+    }  
+    glEnd();
+
+    //Draw Mallet2
+    glBegin(GL_QUAD_STRIP);
+    glColor3f(0.0f, 0.5f, 1.0f);
+    float cx = (c.x - TABLE_WIDTH / 2) * K, cy = (c.y - TABLE_LENGTH / 2) * K;
+    for(double i = 0; i <= 2 * PI; i += PI / 24)
+	{
+		glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + cx, sin(i) * MALLET_DIAMETER * K / 2 + cy, 0.0);
+		glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + cx, sin(i) * MALLET_DIAMETER * K / 2 + cy, 0.1);
+	}
+    glEnd();
+	glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.0f, 0.5f, 1.0f);
+    glVertex3f(cx, cy, 0.0f);
+    glVertex3f(cx, cy, 0.1f);
+    for (double i = 0; i <= 2 * PI; i += PI / 24) 
+    {
+        glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + cx, sin(i) * MALLET_DIAMETER * K / 2 + cy, 0.0);
+		glVertex3f(cos(i) * MALLET_DIAMETER * K / 2 + cx, sin(i) * MALLET_DIAMETER * K / 2 + cy, 0.1);
+    }
+    glEnd();
+
+    //Draw Puck
+    glBegin(GL_POLYGON);
+    glColor3f(1.0f, 0.2f, 0.3f);
+    float bx = (b.x - TABLE_WIDTH / 2) * K, by = (b.y - TABLE_LENGTH / 2) * K;
+    for(double i = 0; i <= 2 * PI; i += PI / 24)
+	{
+		glVertex3f(cos(i) * PUCK_DIAMETER * K / 2 + bx, sin(i) * PUCK_DIAMETER * K / 2 + by, 0.0);
+		glVertex3f(cos(i) * PUCK_DIAMETER * K / 2 + bx, sin(i) * PUCK_DIAMETER * K / 2 + by, 0.1);
+	}
+    glEnd();
+	glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1.0f, 0.2f, 0.3f);
+    glVertex3f(bx, by, 0.0f);
+    glVertex3f(bx, by, 0.1f);
+    for (double i = 0; i <= 2 * PI; i += PI / 24) 
+    {
+        glVertex3f(cos(i) * PUCK_DIAMETER * K / 2 + bx, sin(i) * PUCK_DIAMETER * K / 2 + by, 0.0);
+		glVertex3f(cos(i) * PUCK_DIAMETER * K / 2 + bx, sin(i) * PUCK_DIAMETER * K / 2 + by, 0.1);
+    }
     glEnd();
 
     glutSwapBuffers();
@@ -160,12 +358,17 @@ void reshape (int width, int height) {
 
     glMatrixMode(GL_PROJECTION);                        // Select The Projection Matrix
     glLoadIdentity();                                    // Reset The Projection Matrix
-
+	
     // Calculate The Aspect Ratio Of The Window
     gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
 
+	// Change Look at
+//	gluLookAt(0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	gluLookAt(0.0f, -3.0f, 3.0f, 0.0f, -2.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+
     glMatrixMode(GL_MODELVIEW);                            // Select The Modelview Matrix
-    glLoadIdentity();}
+    glLoadIdentity();
+}
 
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
